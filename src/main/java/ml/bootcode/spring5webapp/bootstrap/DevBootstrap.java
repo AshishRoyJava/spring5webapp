2bf83e7a1f670b8f8d06f964e1007f5fd7350afb
ASHISH-LAPTOP\Ashish Roy <ashishroy077@gmail.com>
2018-04-13 00:46:19 +0530

    Added Publisher Entity and made changes to bootstrap class

--- src/main/java/ml/bootcode/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/ml/bootcode/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package ml.bootcode.spring5webapp.bootstrap;
 
 import ml.bootcode.spring5webapp.model.Author;
 import ml.bootcode.spring5webapp.model.Book;
+import ml.bootcode.spring5webapp.model.Publisher;
 import ml.bootcode.spring5webapp.repositories.AuthorRepository;
 import ml.bootcode.spring5webapp.repositories.BookRepository;
+import ml.bootcode.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -27,7 +31,9 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     private void initData() {
         // Ashish
         Author ashish = new Author("Ashish", "Roy");
-        Book spring = new Book("Spring Framework tutorial", "12345", "Westland");
+        Publisher westland = new Publisher("Westland", "Kolkata");
+        publisherRepository.save(westland);
+        Book spring = new Book("Spring Framework tutorial", "12345", westland);
         ashish.getBooks().add(spring);
         spring.getAuthors().add(ashish);
 
@@ -36,7 +42,9 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         // Soma
         Author soma = new Author("Soma", "Hazra");
-        Book angular = new Book("Angular 4 tutorial", "67890", "Westland");
+        Publisher oriely = new Publisher("Oriely", "Kolkata");
+        publisherRepository.save(oriely);
+        Book angular = new Book("Angular 4 tutorial", "67890", oriely);
         soma.getBooks().add(angular);
 
         authorRepository.save(soma);
